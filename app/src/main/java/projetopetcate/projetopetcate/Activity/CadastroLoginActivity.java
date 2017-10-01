7f1a4a198b593c3f4ff8a8e188f63eed9445d49a
guilh <mrguilher621@gmail.com>
2017-10-01 02:29:35 -0300

    Login inplementado com sucesso

--- app/src/main/java/projetopetcate/projetopetcate/Activity/CadastroLoginActivity.java
+++ app/src/main/java/projetopetcate/projetopetcate/Activity/CadastroLoginActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
